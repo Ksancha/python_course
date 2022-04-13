e9a375b0203c2ce5071933009d53c4f3b886f498
Екатерина Подлужная <ek_dm@mail.ru>
2022-04-14 00:04:42 +0300

    my_first_task

--- exercises/exercise_one/task.py
+++ exercises/exercise_one/task.py
@@ -4,8 +4,22 @@ import os
 def get_list_of_files():
     return os.listdir()
 
+
+print(get_list_of_files())
+
 # Create a function get_pdf_files
 
+
+def get_pdf_files():
+    files = get_list_of_files()
+    result = []
+    for i in files:
+        if i[-3:] == "pdf":
+            result.append(i)
+    return result
+
+
+print(get_pdf_files())
 # Create a function get_csv_files
 
 # Create a function get_jpg_files
