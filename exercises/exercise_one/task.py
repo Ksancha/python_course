dc2bc514dd7c473cdfe9bf601c4a6dbb51a4e6e7
Daniil Balabanov <danedbalabanov@gmail.com>
2022-04-13 20:53:38 +0100

    exercise 1

---
+++ exercises/exercise_one/task.py
@@ -0,0 +1,13 @@
+import os
+
+
+def get_list_of_files():
+    return os.listdir()
+
+# Create a function get_pdf_files
+
+# Create a function get_csv_files
+
+# Create a function get_jpg_files
+
+# Create a single function get_files_by_extension
